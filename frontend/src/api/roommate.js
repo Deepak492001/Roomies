10cbb24ebe6fb0de0b18262d4997d2b8f233668d
Deepak Bisht <86964864+Deepak492001@users.noreply.github.com>
2024-11-05 23:14:07 +0530

    Update roommate.js

--- frontend/src/api/roommate.js
+++ frontend/src/api/roommate.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+import { BASE_URL } from "../constant/constant";
 export const addRoommate = async (room) => {
   try {
     const formData = new FormData();
